43d84f3371387d10a3c9470f5c576ce2ee36cd83
th3br4vetrave1er <tazdingodan@gmail.com>
2024-10-30 18:15:12 +0200

    Well, started adding funcs, but they need to be more elegant, so work on it!

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+path_to_book = "books/frankenstein.txt"
+
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
+    path = path_to_book()
+    with open(path) as book:
+        print(book.read(), word_counter())
+        
     
-    print(file_contents)
+def path_to_book(a="books/frankenstein.txt"):
+    your_path = input("Enter path to book: ")
+    return your_path
 
 
-def counter(text):
-    print(len(text))
+def word_counter(path="books/frankenstein.txt"):
+    words = main()
+    return len(words.split())
+
 
 
 main()
-counter()
